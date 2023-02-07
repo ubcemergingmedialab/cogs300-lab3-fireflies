c24ddb3b9b995283c0f58027de6340443cf3635b
robertshum-eml <robert.shum@ubc.ca>
2023-02-06 18:43:40 -0800

    Revert "GPU Performance Optimizations"
    This reverts commit e5817ef8f55f4d66b59b40a6f7cd2620ab82137f.

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -12,63 +12,63 @@ public class Flock : MonoBehaviour
     [Header("Spawn Setup")]
     public FlockUnit firefly;
     public int spawnNumber = 100;
+    
+    public FlockUnit[] allFireflies {get; set;}
 
-    public FlockUnit[] allFireflies { get; set; }
-
-    [Header("Flashing Setup (Cannot be changed during runtime)")]
+        [Header("Flashing Setup (Cannot be changed during runtime)")]
 
     [Range(0, 10)]
     [SerializeField] private int _sightDistance;
-    public int sightDistance { get { return _sightDistance; } }
+    public int sightDistance {get {return _sightDistance;}}
 
     [Range(0, 5)]
     [SerializeField] private float _delayMultiplier;
-    public float delayMultiplier { get { return _delayMultiplier; } }
+    public float delayMultiplier {get {return _delayMultiplier;}}
 
 
     [Header("Speed Setup")]
     [Range(0, 10)]
     [SerializeField] private float _minSpeed;
-    public float minSpeed { get { return _minSpeed; } }
+    public float minSpeed {get {return _minSpeed;}}
     [Range(0, 10)]
     [SerializeField] private float _maxSpeed;
-    public float maxSpeed { get { return _maxSpeed; } }
+    public float maxSpeed {get {return _maxSpeed;}}
 
-
+   
     [Header("Detection Distances")]
     [Header("--------Flocking Behaviours-----------")]
 
-
+   
     [Range(0, 10)]
     [SerializeField] private float _cohesionDistance;
-    public float cohesionDistance { get { return _cohesionDistance; } }
+    public float cohesionDistance {get {return _cohesionDistance;}}
     [Range(0, 10)]
     [SerializeField] private float _alignmentDistance;
-    public float alignmentDistance { get { return _alignmentDistance; } }
+    public float alignmentDistance {get {return _alignmentDistance;}}
     [Range(0, 10)]
     [SerializeField] private float _avoidanceDistance;
-    public float avoidanceDistance { get { return _avoidanceDistance; } }
+    public float avoidanceDistance {get {return _avoidanceDistance;}}
 
-
+    
 
 
     [Range(0, 10)]
     [SerializeField] private float _boundsDistance;
-    public float boundsDistance { get { return _boundsDistance; } }
+    public float boundsDistance {get {return _boundsDistance;}}
 
     [Header("Behaviour Weights")]
     [Range(0, 10)]
     [SerializeField] private float _cohesionWeight;
-    public float cohesionWeight { get { return _cohesionWeight; } }
+    public float cohesionWeight {get {return _cohesionWeight;}}
     [Range(0, 10)]
     [SerializeField] private float _alignmentWeight;
-    public float alignmentWeight { get { return _alignmentWeight; } }
+    public float alignmentWeight {get {return _alignmentWeight;}}
     [Range(0, 10)]
     [SerializeField] private float _avoidanceWeight;
-    public float avoidanceWeight { get { return _avoidanceWeight; } }
+    public float avoidanceWeight {get {return _avoidanceWeight;}}
     [Range(0, 10)]
     [SerializeField] private float _boundsWeight;
-    public float boundsWeight { get { return _boundsWeight; } }
+    public float boundsWeight {get {return _boundsWeight;}}
 
     void Start()
     {
@@ -78,54 +78,18 @@ public class Flock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveFireflies();
-    }
-
-    private void MoveFireflies()
-    {
-        for (int i = 0; i < allFireflies.Length; i++)
-        {
-            //reset the vectors for the current FF
-            allFireflies[i].ResetVectors();
-
-            //clear neighbours
-            allFireflies[i].ClearNeighbours();
-
-            for (int j = i; j < allFireflies.Length; j++)
-            {
-                //we don't need to look at the same FF
-                if (i != j)
-                {
-                    float currentNeighbourDistanceSqr = Vector3.SqrMagnitude(allFireflies[j].transform.position - allFireflies[i].transform.position);
-                    if (currentNeighbourDistanceSqr <= cohesionDistance * cohesionDistance)
-                    {
-                        allFireflies[i].AddCohesionNeighbour(allFireflies[j]);
-                        allFireflies[j].AddCohesionNeighbour(allFireflies[i]);
-                    }
-                    if (currentNeighbourDistanceSqr <= alignmentDistance * alignmentDistance)
-                    {
-                        allFireflies[i].AddAlignmentNeighbours(allFireflies[j]);
-                        allFireflies[j].AddAlignmentNeighbours(allFireflies[i]);
-                    }
-                    if (currentNeighbourDistanceSqr <= avoidanceDistance * avoidanceDistance)
-                    {
-                        allFireflies[i].AddAvoidanceNeighbours(allFireflies[j]);
-                        allFireflies[j].AddAvoidanceNeighbours(allFireflies[i]);
-                    }
-                }
-            }
+        for(int i = 0; i < allFireflies.Length; i++){
             allFireflies[i].MoveUnit();
+
         }
     }
 
-    private void GenerateFireflies()
-    {
+    private void GenerateFireflies(){
         allFireflies = new FlockUnit[spawnNumber];
-        for (int i = 0; i < spawnNumber; i++)
-        {
+        for(int i = 0; i < spawnNumber; i++){
             FlockUnit newFirefly = Instantiate(firefly);
             allFireflies[i] = newFirefly;
-            newFirefly.transform.position = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20));
+            newFirefly.transform.position = new Vector3(Random.Range(-20, 20),Random.Range(-20, 20),Random.Range(-20, 20));
             newFirefly.transform.rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
             newFirefly.transform.parent = this.transform;
             FireflyLights lightsScript = newFirefly.GetComponent<FireflyLights>();
--- Assets/Scripts/FlockUnit.cs
+++ Assets/Scripts/FlockUnit.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FlockUnit : MonoBehaviour
 {
+
     [SerializeField] private float smoothDamp;
     private List<FlockUnit> cohesionNeighbours = new List<FlockUnit>();
     private List<FlockUnit> alignmentNeighbours = new List<FlockUnit>();
@@ -11,30 +12,23 @@ public class FlockUnit : MonoBehaviour
     private Flock assignedFlock;
     private Vector3 currentVelocity;
     private float speed;
-    private Vector3 avoidanceVector;
-    private Vector3 alignmentVector;
-    private Vector3 cohesionVector;
 
-    public Transform myTransform { get; set; }
+    public Transform myTransform {get; set;}
 
-    private void Awake()
-    {
+    private void Awake(){
         myTransform = transform;
     }
 
-    public void AssignFlock(Flock flock)
-    {
+    public void AssignFlock(Flock flock){
         assignedFlock = flock;
     }
 
-    public void InititializeSpeed(float speed)
-    {
+    public void InititializeSpeed(float speed){
         this.speed = speed;
     }
 
-
-    public void MoveUnit()
-    {
+    public void MoveUnit(){
+        FindNeighbours();
         CalculateSpeed();
 
         var cohesionVector = CalculateCohesionVector() * assignedFlock.cohesionWeight;
@@ -42,66 +36,60 @@ public class FlockUnit : MonoBehaviour
         var alignmentVector = CalculateAlignmentVector() * assignedFlock.alignmentWeight;
         var boundsVector = CalculateBoundsVector() * assignedFlock.boundsWeight;
 
+
         var moveVector = cohesionVector + avoidanceVector + alignmentVector + boundsVector;
         moveVector = Vector3.SmoothDamp(myTransform.forward, moveVector, ref currentVelocity, smoothDamp);
         moveVector = moveVector.normalized * speed;
-        if (moveVector != Vector3.zero)
-        {
+        if(moveVector != Vector3.zero){
             myTransform.forward = moveVector;
         }
-
+        
         myTransform.position += moveVector * Time.deltaTime;
     }
 
-    public void AddCohesionNeighbour(FlockUnit flockUnit)
-    {
-        cohesionNeighbours.Add(flockUnit);
-        speed += flockUnit.speed;
-    }
-
-    public void AddAlignmentNeighbours(FlockUnit flockUnit)
-    {
-        alignmentNeighbours.Add(flockUnit);
-        alignmentVector += flockUnit.myTransform.forward;
-    }
-
-    public void AddAvoidanceNeighbours(FlockUnit flockUnit)
-    {
-        avoidanceNeighbours.Add(flockUnit);
-        avoidanceVector += (myTransform.position - flockUnit.myTransform.position);
-    }
-
-    public void ResetVectors()
-    {
-        speed = 0;
-        avoidanceVector = Vector3.zero;
-        alignmentVector = myTransform.forward;
-        cohesionVector = Vector3.zero;
-    }
-
-    public void ClearNeighbours()
-    {
+    private void FindNeighbours(){
         cohesionNeighbours.Clear();
         alignmentNeighbours.Clear();
         avoidanceNeighbours.Clear();
+        var allFireflies = assignedFlock.allFireflies;
+        for (int i = 0; i < assignedFlock.spawnNumber; i++){
+            var currentUnit = allFireflies[i];
+            if(currentUnit != this){
+                float currentNeighbourDistanceSqr = Vector3.SqrMagnitude(currentUnit.transform.position - transform.position);
+                if(currentNeighbourDistanceSqr <= assignedFlock.cohesionDistance * assignedFlock.cohesionDistance){
+                    cohesionNeighbours.Add(currentUnit);
+                }
+                if(currentNeighbourDistanceSqr <= assignedFlock.alignmentDistance * assignedFlock.alignmentDistance){
+                    alignmentNeighbours.Add(currentUnit);
+                }
+                if(currentNeighbourDistanceSqr <= assignedFlock.avoidanceDistance * assignedFlock.avoidanceDistance){
+                    avoidanceNeighbours.Add(currentUnit);
+                }
+            }
+        }
     }
 
-    private void CalculateSpeed()
-    {
-        if (cohesionNeighbours.Count == 0)
-        {
+    private void CalculateSpeed(){
+        speed = 0;
+        if(cohesionNeighbours.Count == 0){
             speed = assignedFlock.minSpeed;
             return;
+        }
+        for (int i = 0; i < cohesionNeighbours.Count; i++){
+            speed += cohesionNeighbours[i].speed;
         }
         speed /= cohesionNeighbours.Count;
         speed = Mathf.Clamp(speed, assignedFlock.minSpeed, assignedFlock.maxSpeed);
     }
 
-    private Vector3 CalculateAvoidanceVector()
-    {
-        if (avoidanceNeighbours.Count == 0)
-        {
+    private Vector3 CalculateAvoidanceVector(){
+        var avoidanceVector = Vector3.zero;
+        if(avoidanceNeighbours.Count == 0){
             return avoidanceVector;
+        }
+        for (int i = 0; i < avoidanceNeighbours.Count; i++)
+        {
+            avoidanceVector += (myTransform.position - avoidanceNeighbours[i].myTransform.position);
         }
         avoidanceVector /= avoidanceNeighbours.Count;
         avoidanceVector = avoidanceVector.normalized;
@@ -109,12 +97,14 @@ public class FlockUnit : MonoBehaviour
         return avoidanceVector;
     }
 
-
-    private Vector3 CalculateAlignmentVector()
-    {
-        if (alignmentNeighbours.Count == 0)
-        {
+    private Vector3 CalculateAlignmentVector(){
+        var alignmentVector = myTransform.forward;
+        if(alignmentNeighbours.Count == 0){
             return alignmentVector;
+        }
+        for (int i = 0; i < alignmentNeighbours.Count; i++)
+        {
+            alignmentVector += alignmentNeighbours[i].myTransform.forward;
         }
         alignmentVector /= alignmentNeighbours.Count;
         alignmentVector = alignmentVector.normalized;
@@ -122,20 +112,21 @@ public class FlockUnit : MonoBehaviour
         return alignmentVector;
     }
 
-    private Vector3 CalculateBoundsVector()
-    {
+    private Vector3 CalculateBoundsVector(){
         var offsetToCenter = assignedFlock.transform.position - myTransform.position;
         bool isNearCenter = (offsetToCenter.magnitude >= assignedFlock.boundsDistance * 0.9f);
-        return isNearCenter ? offsetToCenter.normalized : Vector3.zero;
+		return isNearCenter ? offsetToCenter.normalized : Vector3.zero;
     }
 
-    private Vector3 CalculateCohesionVector()
-    {
-        if (cohesionNeighbours.Count == 0)
-        {
+    private Vector3 CalculateCohesionVector(){
+        var cohesionVector = Vector3.zero;
+
+        if(cohesionNeighbours.Count == 0){
             return cohesionVector;
         }
-
+        for(int i = 0; i < cohesionNeighbours.Count; i++){
+            cohesionVector += cohesionNeighbours[i].myTransform.position;
+        }
         cohesionVector /= cohesionNeighbours.Count;
         cohesionVector -= myTransform.position;
         cohesionVector = cohesionVector.normalized;
